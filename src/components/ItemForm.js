939c2c066ed5236d3a4cb7bb2c5690d063acdd0c
samuelconlon <conlonsp@yahoo.com>
2022-09-23 17:19:25 -0400

    commit tree b30b5f2b1c23e8bc47d7c472c0809c456704c9d9

--- src/components/ItemForm.js
+++ src/components/ItemForm.js
@@ -1,11 +1,29 @@
 import React, { useState } from "react";
 
-function ItemForm() {
+function ItemForm({onAddItem}) {
   const [name, setName] = useState("");
   const [category, setCategory] = useState("Produce");
 
+  function handleSubmit(event) {
+    event.preventDefault()
+    const newItem = {
+      name: name,
+      category: category,
+      isInCart: false
+    }
+    fetch('http://localhost:4000/items', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newItem),
+    })
+    .then(r => r.json())
+    .then(data => onAddItem(data))
+  }
+
   return (
-    <form className="NewItem">
+    <form onSubmit={handleSubmit} className="NewItem">
       <label>
         Name:
         <input
